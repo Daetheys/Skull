3ea955d200e804214a3c8f10729db32841190dc6
Daetheys <daetheys@gmail.com>
2019-11-30 15:23:17 +0100

    Added comments

--- mini/instance.py
+++ mini/instance.py
@@ -1,5 +1,6 @@
 from mini import Player,Game
 
+"""Creation of player """
 class Player1(Player):
     def play(self):
         return self.hand[0]
@@ -10,9 +11,13 @@ class Player1(Player):
     def turn_cards(self):
         return [self.game.players.index(self)]
 
+#Instances of players
 playerA = Player1()
 playerB = Player1()
 
+#Creates the game
 g = Game([playerA,playerB])
+
+#Launch 2 rounds
 g.round()
 g.round()
--- mini/mini.py
+++ mini/mini.py
@@ -7,32 +7,35 @@ class Won(Exception):
         pass
 
 class Player:
+    """ Player class """
     def __init__(self):
-        self.hand = [1,0,0,0]
+        self.hand = [1,0,0,0] #1-> Skull // 0-> Flower
         self.won = 0
 
     def link(self,game):
+        #Have a link to the game configuration
         self.game = game
 
-    def play(self):
+    def play(self): #To define -> play a card (0 or 1)
         pass
 
-    def bid(self):
+    def bid(self): #To define -> bid 
         pass
 
-    def turn_cards(self):
+    def turn_cards(self): #To define -> Turn cards (needs to be the same number of cards than the last bid and start with cards of this player
         pass
 
-    def lose(self):
+    def lose(self): #Player loses
         r = random.randint(0,len(self.hand)-1)
         del self.hand[r]
 
-    def win(self):
+    def win(self): #Player wins
         self.won += 1
         if self.won >= 2:
             raise Won(self)
 
 class Game:
+    """ Game class """
     def __init__(self,players):
         self.players = players
         self.played = [None]*len(players)
@@ -42,13 +45,18 @@ class Game:
             p.link(self)
 
     def round(self):
+        """ Start a round """
+        #Play round
         for i in range(len(self.players)):
             self.play(i)
+        #Bid round
         for i in range(len(self.players)):
             self.bid(i)
+        #Get result of bid and turn cards
         bidder = self.players[self.player_bid]
         cards = bidder.turn_cards()
         print(self.player_bid,"turned",cards)
+        #Verify if the bidder wins or loses
         if sum([self.played[i] for i in cards]) >= 1:
             print(self.player_bid,"lost round")
             bidder.lose()
@@ -57,10 +65,12 @@ class Game:
             bidder.win()
         
     def play(self,i):
+        """ i will play """
         self.played[i] = self.players[i].play()
         print(i,"played",self.played[i])
 
     def bid(self,i):
+        """ i will bid """
         bid = self.players[i].bid()
         if bid > self.best_bid:
             print(i,"bet",bid)
